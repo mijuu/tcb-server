fbffc0888006a07921103ae6cff46126097969f8
Raymond <raymond.liang@outlook.com>
2020-09-02 09:30:13 +0800

    BaseContextClass中绑定middleware

--- src/base-context-class.js
+++ src/base-context-class.js
@@ -4,8 +4,9 @@ class BaseContextClass {
   constructor (app) {
     this.app = app;
     this.ctx = app.ctx;
-    this.controller = app.controller
+    this.controller = app.controller;
     this.service = app.service;
+    this.middleware = app.middleware;
     this.cloud = app.cloud;
   }
 }
